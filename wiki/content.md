c3305210219305e4d5e618cabf1df2123aad677d
ahaggart <alex.haggart@gmail.com>
2018-08-01 21:15:54 -0700

    moved js to a raw github content, configured markdown renderer

--- wiki/content.md
+++ wiki/content.md
@@ -1,1 +1,7 @@
-#Hello Markdown!
+---
+title: Yonder Home
+---
+# Yonder Wiki Home
+This page is currently [under construction](https://gitlab.com/Yonder-Dynamics/wiki-home).
+
+Eventually it will be the home page for the Yonder Software wiki.
--- wiki/main.js
+++
@@ -1,9 +0,0 @@
-var client = new XMLHttpRequest();
-client.open('GET', 'content.md');
-client.onreadystatechange = function() {
-    var converter = new showdown.Converter(),
-    text      = client.responseText,
-    html      = converter.makeHtml(text);
-    document.getElementById("root").innerHTML = html;
-}
-client.send();
